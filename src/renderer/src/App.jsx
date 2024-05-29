ab596a5fcadfee18c17817ae956d97c2b0af8a71
Tom Sabu <tomsabu0000@gmail.com>
2024-05-29 12:37:23 +0530

    material UI --added

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+//? imports
+import ImageGenetaion from './components/ImageGeneration'
 function App() {
   return (
-    <div>App</div>
+    <>
+    <ImageGenetaion/>
+    </>
   )
 }
 
