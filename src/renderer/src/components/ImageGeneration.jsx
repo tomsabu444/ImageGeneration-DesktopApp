5f66ad3132cc7f85a2264503ad0f94fa59de8cfb
Tom Sabu <tomsabu0000@gmail.com>
2024-05-29 12:25:06 +0530

    ImageGeneration --int

---
+++ src/renderer/src/components/ImageGeneration.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ImageGeneration() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="image-promte-box">
+          <input type="text"  placeholder='Describe your image..'/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ImageGeneration
