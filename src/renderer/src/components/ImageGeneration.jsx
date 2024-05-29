24f4fda243886c9c9b80ed12e7627e9df13884e0
Tom Sabu <tomsabu0000@gmail.com>
2024-05-29 13:03:45 +0530

    input text area

--- src/renderer/src/components/ImageGeneration.jsx
+++ src/renderer/src/components/ImageGeneration.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-
+import './styles/ImageGeneration.css'
+import SendIcon from '@mui/icons-material/Send'
+import { TextareaAutosize } from '@mui/material'
 function ImageGeneration() {
   return (
     <>
       <div className="main-container">
         <div className="image-promte-box">
-          <input type="text"  placeholder='Describe your image..'/>
+          <div className="input-place">
+            <TextareaAutosize
+              id="inputpost"
+              placeholder="What's happening?!"
+              className="send-input"
+              autoFocus
+            />
+
+            <div className="send">
+              <SendIcon className="send-icon" />
+            </div>
+          </div>
         </div>
       </div>
     </>
