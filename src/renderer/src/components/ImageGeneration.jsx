a3d1b379e1eaac47b3ea221bec8a0208ac0be036
TOM SABU <tomsabu0000@gmail.com>
2024-05-30 23:30:30 +0530

    "Updated ImageGeneration component and styles: added img element with src attribute, added .show-image class, and modified existing styles"

--- src/renderer/src/components/ImageGeneration.jsx
+++ src/renderer/src/components/ImageGeneration.jsx
@@ -5,8 +5,13 @@ import { TextareaAutosize } from '@mui/material'
 function ImageGeneration() {
   return (
     <>
-      <div className="main-container">
-        .show imag
+      <div className="main">
+        <div className="show-image">
+          {/* <img
+            src="https://cdn.discordapp.com/attachments/940898967959523358/1090662952207654973/background.gif?ex=6659f087&is=66589f07&hm=aee0b54dce1c7f5a628b9aafd76dfed33de298ee278bf3f9d7c953abb64a141d&"
+            alt=""
+          /> */}
+        </div>
         <div className="image-promte-box">
           <div className="input-place">
             <TextareaAutosize
